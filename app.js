05719692add99f7a4134e9ba32c7e31a115e343e
toralos <maxi.abramchuk@gmail.com>
2020-12-05 18:35:00 -0300

    Aca agrego carpeta Styles

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Hola Mundo")
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', function (req,res){
+    
+    res.sendFile(path.join(__dirname, './index.html'))
+})
+
+app.get('/login', function(req,res){
+    res.sendFile(path.join(__dirname, './login.html'))
+
+})
+
+app.get('/register', function(req,res){
+    res.sendFile(path.join(__dirname, './register.html'))
+
+})
+app.get('/carrito-compras', function(req,res){
+    res.sendFile(path.join(__dirname, './carrito-compras.html'))
+
+})
+app.get('/productos', function(req,res){
+    res.sendFile(path.join(__dirname, './productos.html'))
+
+})
+app.get('/detalles-producto', function(req,res){
+    res.sendFile(path.join(__dirname, './detalles-producto.html'))
+
+})
+
+app.listen(3000, function(){
+    console.log("El servidor esta corriendo en el servidor 3000")
+})
