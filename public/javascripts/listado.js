b8f15ffbca940caec0030243cd2e51048e02a330
Francisco Csik <francsik23@gmail.com>
2020-12-11 11:00:19 -0300

    terminado listado en todos los viewports

--- public/javascripts/listado.js
+++ public/javascripts/listado.js
@@ -1,8 +1,19 @@
 let filtrosDiv = document.querySelector(".filtros");
-
 let todosFiltros = document.getElementById('todos-filtros');
 
+/*PARA QUE EL USARIO TENGA QUE APRETAR Y PODER DESPLEGAR LOS FILTROS*/ 
 
+function filtrarBusqueda(){
 filtrosDiv.addEventListener('click', () => {
-    todosFiltros.style.display !== 'none' ? todosFiltros.style.display = 'none' : todosFiltros.style.display = 'block' ;
+    if ( todosFiltros.style.display !== 'none' ) {
+        todosFiltros.style.display = 'none'
+    }else {
+        todosFiltros.style.display = 'block'
+    }
 })
+};
+
+/*=====
+APLICAMOS ESTA FUNCION SOLO SI LA PANTALLA ES MENOR A 1440px 
+=====*/ 
+screen.width > 1440 ? todosFiltros.style.display = 'block' : filtrarBusqueda();
