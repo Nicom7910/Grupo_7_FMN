4f00546e51cb4c3823bf6881765dab2e925cc93b
Your Name[D[DNicolas <nicolasmaidana74@gmail.com>
2021-01-27 17:17:19 -0300

    Cambios

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -15,7 +15,7 @@ module.exports = {
         res.render('carrito')
     },
     login: (req, res) => {
-        res.render('login-responsive')
+        res.render('login')
     },
     register: (req, res) => {
         res.render('register')
