c1da589483e98e4008a93232322f67413cebb78e
Your Name[D[DNicolas <nicolasmaidana74@gmail.com>
2021-03-19 14:10:02 -0300

    Validacion l/r

--- public/validations/formValidator.js
+++ public/validations/formValidator.js
@@ -8,47 +8,73 @@ const loguear = document.getElementById("login");
 const emailL = document.getElementById("emailL");
 const contraseñaL = document.getElementById("contraseña");
 const form_login = document.getElementById("form-login");
+const error = document.getElementsByClassName('error');
+const mostrarError = document.querySelector('div.error');
+
+window.onload= function(){}
 
 // Formulario de Registro
-window.addEventListener("load", function(){
-    registrar.addEventListener("click", function(event){
-        event.preventDefault();    
-        if (nombreR.value == ""){
-            alert('Completa el campo Nombre');
-        }
-        else if (emailR.value == ""){
-            alert('Completa el campo Email');
-        }
-        else if (!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i).test(emailR.value)){ //Expresion Regular para caracteres Unicode
-            alert('Ingrese un email valido');
-        }
-        else if (contraseñaR.value.length < 8 ){
-            alert('La Contraseña debe tener mas de 8 caracteres');
-        }
-        else if (!(/\.(jpg|jpeg|png|gif)$/i).test(avatarR.value) ) {
-            alert('Formato de imagen incorrecto. Debe ser .jpg/.jpeg/.png/.gif');
-        }
-        else{
-            form_register.submit();
-        }
-    })
+
+let mensajesError = [];
+registrar.addEventListener("click", function(event){
+    //event.preventDefault();
+    if (nombreR.value == "" || nombreR.value.length < 2){
+        //mensajesError.push('Completa el campo Nombre');
+        //mostrarError.innerHTML = mensajesError;
+        alert('El campo Nombre necesita minimo 3 caracteres');
+    }
+    else if (emailR.value == ""){
+        //mensajesError.push('Completa el campo Email')
+        //mostrarError.innerHTML = mensajesError;
+        alert('Completa el campo Email');
+    }
+    else if (!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i).test(emailR.value)){ //Expresion Regular para caracteres Unicode
+        //mensajesError.push('Ingrese un email valido');
+        //mostrarError.innerHTML = mensajesError;
+        alert('Ingrese un email valido');
+    }
+    else if (contraseñaR.value.length < 8 ){
+        //mensajesError.push('La Contraseña debe tener mas de 8 caracteres');
+        //mostrarError.innerHTML = mensajesError;
+        alert('La Contraseña debe tener mas de 8 caracteres');
+    }
+    else if (!(/\.(jpg|jpeg|png|gif)$/i).test(avatarR.value) ) {
+        //mensajesError.push('Formato de imagen incorrecto. Debe ser .jpg/.jpeg/.png/.gif');
+        //mostrarError.innerHTML = mensajesError;
+        alert('Formato de imagen incorrecto. Debe ser .jpg/.jpeg/.png/.gif');
+    }
+    else if (mensajesError.length !=0){
+        //console.log('Errrroooooorrrr')
+        event.preventDefault();
+    }
+    else{
+        form_register.submit();
+    }
 })
 
 // Formulario de Login
-window.addEventListener("load", function(){
-    loguear.addEventListener("click", function(event){
-        event.preventDefault();    
-        if(emailL.value == ""){
-            alert('Completa el campo Email');
-        }
-        else if (!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i).test(emailL.value)){ //Expresion Regular para caracteres Unicode
-            alert('Ingrese un email valido');
-        }
-        else if (contraseñaL.value.length < 8){
-            alert('La Contraseña debe tener mas de 8 caracteres');
-        }
-        else{
-            form_login.submit();
-        }
-    })
+loguear.addEventListener("click", function(event){
+    //event.preventDefault();
+    if(emailL.value == ""){
+        //mensajesError.push('Completa el campo Email');
+        //mostrarError.innerHTML = mensajesError;
+        alert('Completa el campo Email');
+    }
+    else if (!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i).test(emailL.value)){ //Expresion Regular para caracteres Unicode
+        //mensajesError.push('Ingrese un email valido');
+        //mostrarError.innerHTML = mensajesError;
+        alert('Ingrese un email valido');
+    }
+    else if (contraseñaL.value.length < 8){
+        //mensajesError.push('La Contraseña debe tener mas de 8 caracteres');
+        //mostrarError.innerHTML = mensajesError;
+        alert('La Contraseña debe tener mas de 8 caracteres');
+    }
+    else if (mensajesError.length !=0){
+        //console.log('hay error')
+        event.preventDefault();
+    }
+    else{
+        form_login.submit();
+    }
 })
--- src/middlewares/registerValidation.js
+++ src/middlewares/registerValidation.js
@@ -4,7 +4,9 @@ module.exports = [
     check('nombre')
         .isLength( {min: 2} ).withMessage("El nombre debe tener mas de 2 caracteres.") ,
     check('email')
-        .isEmail().withMessage('Tenés que ingresar un email valido.'),     
+        .isEmail().withMessage('Tenés que ingresar un email valido.'),
+    check('avatar')
+        .isEmpty().withMessage('Debe elegir una imagen'),    
     check('password')
         .isLength( {min: 8} ).withMessage('La contraseña debe de tener mas de 8 caracteres.')
 ]
