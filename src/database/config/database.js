9fb2f1f21ecb5db10e6bb7cc2e44164c0c49733e
Your Name[D[DNicolas <nicolasmaidana74@gmail.com>
2021-03-23 22:41:26 -0300

    Heroku y env

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
   "production": {
-    "username": "fmn",
-    "password": "Bostero123",
-    "database": "fmn_electronica",
-    "host": "mysql-fmn.alwaysdata.net",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
