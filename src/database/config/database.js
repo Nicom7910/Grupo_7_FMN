a553f40826528b6fbaef03a5c022f697e6e37ad3
Your Name[D[DNicolas <nicolasmaidana74@gmail.com>
2021-03-24 13:21:06 -0300

    .

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -18,7 +18,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "database_development",
+    "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
